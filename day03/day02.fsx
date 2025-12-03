c8ac2d6903b7f0b210b1d195d582079f7bfd393c
Nicholas Edwards <edwardsnjd@googlemail.com>
2025-12-03 17:29:09 +0800

    refactor: Minor refactor to prefer composition
    Just seeing how comfortable it feels to use composition rather than pipelines
    for most of the flow.

--- day03/day02.fsx
+++ day03/day02.fsx
@@ -10,23 +10,19 @@ let maxWithIndex = List.indexed >> List.sortByDescending snd >> List.head
 let toDigits = Seq.map (string >> int) >> Seq.toList
 let fromDigits = List.map string >> String.concat "" >> int64
 
-let joltage numDigits line =
-    let rec maxNDigits (n: int) (digits: int list) : int list =
-        if n < 1 then
-            []
-        else
-            let pool = List.take (List.length digits - n + 1) digits
-            let indexOfMax, maxValue = maxWithIndex pool
-            let digits' = List.skip (indexOfMax + 1) digits in
-            maxValue :: maxNDigits (n - 1) digits'
+let rec maxNDigits n digits =
+    if n < 1 then
+        []
+    else
+        let pool = List.take (List.length digits - n + 1) digits
+        let indexOfMax, maxValue = maxWithIndex pool
+        let digits' = List.skip (indexOfMax + 1) digits in
+        maxValue :: maxNDigits (n - 1) digits'
 
-    line |> toDigits |> maxNDigits numDigits |> fromDigits
+let joltage numDigits = toDigits >> maxNDigits numDigits >> fromDigits
 
-let part1 lines =
-    lines |> List.map (joltage 2) |> List.sum
-
-let part2 lines =
-    lines |> List.map (joltage 12) |> List.sum
+let part1 = List.map (joltage 2) >> List.sum
+let part2 = List.map (joltage 12) >> List.sum
 
 lines |> part1 |> printfn "Part1: %A"
 lines |> part2 |> printfn "Part2: %A"
