04d3df6bb4de1a87014c6879e0f26df90f1ee5a6
Nicholas Edwards <edwardsnjd@googlemail.com>
2025-12-12 00:53:49 +0800

    refactor: Day10 with the core loop switched to counts

--- day10/day10.fsx
+++ day10/day10.fsx
@@ -2,43 +2,43 @@
 
 open System.Text.RegularExpressions
 
-let lines =
-    Seq.initInfinite (fun _ -> System.Console.ReadLine())
-    |> Seq.takeWhile (fun line -> line <> null)
-    |> Seq.toList
-
-let words (line: string) =
-    line.Split(',', System.StringSplitOptions.RemoveEmptyEntries)
+type Slots = int list
+type Machine = int list * Set<int list>
 
 let fooRegex = Regex @"^\[(.*)\] (.*) \{(.*)\}$"
 
-let parse line =
+let parse line : Machine =
     let m = fooRegex.Match line
 
     let lights = m.Groups.[1].Value
     let count = String.length lights
-    let bitValue i = 1 <<< count - i - 1
 
     let a =
         lights
-        |> Seq.mapi (fun i c ->
-            match c with
+        |> Seq.map (function
             | '.' -> 0
-            | '#' -> bitValue i
+            | '#' -> 1
             | _ -> failwith "nope")
-        |> Seq.fold (+) 0
+        |> Seq.toList
 
     let b =
         m.Groups.[2].Value.Split ' '
         |> Array.map (fun s -> s.[1 .. s.Length - 2])
-        |> Array.map (fun s -> s.Split ',')
-        |> Array.map (Array.map int)
-        |> Array.map (Array.map bitValue >> Seq.fold (+) 0)
+        |> Array.map (fun s -> s.Split ',' |> Array.map int)
+        |> Array.map (fun vs ->
+            [ for i in 0 .. count - 1 do
+                  yield if Array.contains i vs then 1 else 0 ])
         |> Set.ofArray
 
     // let c = m.Groups.[3].Value
 
     a, b
+
+let lines =
+    Seq.initInfinite (fun _ -> System.Console.ReadLine())
+    |> Seq.takeWhile (fun line -> line <> null)
+    |> Seq.map parse
+    |> Seq.toList
 
 let rec combinationsOfN buttons n =
     seq {
@@ -62,16 +62,23 @@ let allCombos buttons =
             yield! combinationsOfN buttonsL n
     }
 
-let step lights button = lights ^^^ button
-let result buttons = Seq.fold step 0 buttons
+let flip fn a b = fn b a
 
-let solve (target, buttons) =
+let step lights button =
+    List.zip lights button |> List.map (fun (a, b) -> a + b)
+
+let result count buttons =
+    let initial = List.replicate count 0
+    Seq.fold step initial buttons
+
+let solve ((target, buttons): Machine) =
+    let count = List.length target
+
     allCombos buttons
-    |> Seq.filter (result >> (=) target)
+    |> Seq.filter (result count >> List.map (flip (%) 2) >> (=) target)
     |> Seq.head
     |> List.length
 
-let part1 lines =
-    List.map parse lines |> List.map solve |> List.sum
+let part1 machines = machines |> List.map solve |> List.sum
 
 lines |> part1 |> printfn "Part1: %A"
