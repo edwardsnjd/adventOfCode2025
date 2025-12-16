2b0b2853ff12e09ab66f997568e8e2d82681bcab
Nicholas Edwards <edwardsnjd@googlemail.com>
2025-12-16 14:39:31 +0800

    wip

--- day10/day10.fsx
+++ day10/day10.fsx
@@ -3,7 +3,8 @@
 open System.Text.RegularExpressions
 
 type Slots = int list
-type Machine = int list * Set<int list>
+type Buttons = int list list
+type Machine = Slots * Buttons * Slots
 
 let fooRegex = Regex @"^\[(.*)\] (.*) \{(.*)\}$"
 
@@ -28,11 +29,11 @@ let parse line : Machine =
         |> Array.map (fun vs ->
             [ for i in 0 .. count - 1 do
                   yield if Array.contains i vs then 1 else 0 ])
-        |> Set.ofArray
+        |> Array.toList
 
-    // let c = m.Groups.[3].Value
+    let c = m.Groups.[3].Value.Split ',' |> Array.map int |> Array.toList
 
-    a, b
+    a, b, c
 
 let lines =
     Seq.initInfinite (fun _ -> System.Console.ReadLine())
@@ -40,7 +41,7 @@ let lines =
     |> Seq.map parse
     |> Seq.toList
 
-let rec combinationsOfN buttons n =
+let rec combinationsOfN buttons n : Buttons seq =
     seq {
         if n = 0 then
             yield []
@@ -54,31 +55,107 @@ let rec combinationsOfN buttons n =
                 yield! combinationsOfN rest n
     }
 
-let allCombos buttons =
-    let buttonsL = Set.toList buttons
-
+let rec combinationsOfNPresses buttons n : Buttons seq =
     seq {
-        for n in 0 .. buttonsL.Length do
-            yield! combinationsOfN buttonsL n
+        if n = 0 then
+            yield []
+        else
+            for i in 0 .. List.length buttons - 1 do
+                let button = buttons.[i]
+
+                for restCombos in combinationsOfNPresses buttons (n - 1) do
+                    yield button :: restCombos
     }
 
-let flip fn a b = fn b a
+let allCombos (buttons: Buttons) : Buttons seq =
+    seq {
+        for n in 0 .. buttons.Length do
+            yield! combinationsOfN buttons n
+    }
 
 let step lights button =
     List.zip lights button |> List.map (fun (a, b) -> a + b)
 
-let result count buttons =
-    let initial = List.replicate count 0
-    Seq.fold step initial buttons
+let after buttons initial : Slots = Seq.fold step initial buttons
 
-let solve ((target, buttons): Machine) =
+let emptySlots count = List.replicate count 0
+
+let result count buttons : Slots = after buttons (emptySlots count)
+
+let modulus v n = n % v
+
+let solve1 ((target, buttons, _): Machine) =
     let count = List.length target
 
     allCombos buttons
-    |> Seq.filter (result count >> List.map (flip (%) 2) >> (=) target)
+    |> Seq.filter (result count >> List.map (modulus 2) >> (=) target)
     |> Seq.head
     |> List.length
 
-let part1 machines = machines |> List.map solve |> List.sum
+let tap fn x =
+    fn x
+    x
+
+let isValidFor (target: Slots) (result: Slots) =
+    List.zip target result |> List.forall (fun (a, b) -> a >= b)
+
+let solve2 ((_, buttons, joltage): Machine) : int =
+    printfn "joltage:\n%A" joltage
+    printfn "buttons:\n%A" buttons
+
+    let count = List.length joltage
+
+    let rec possiblePresses (target: Slots) idx (current: Slots) : Buttons seq =
+        printfn "possiblePresses %A %A %A" target idx current
+        let length = target.Length
+
+        seq {
+            if idx >= length then
+                yield []
+            else
+                let numberOfPressesNeeded = target.[idx] - current.[idx]
+                let solvedIndices = List.filter (fun i -> target.[i] = 0) [ 0 .. length - 1 ]
+                let affectsIndex i button = List.item i button |> (=) 1
+
+                let relevantButtons =
+                    buttons
+                    |> List.filter (affectsIndex idx)
+                    |> List.filter (fun b -> solvedIndices |> List.forall (fun i -> not (affectsIndex i b)))
+                    |> List.sortByDescending List.length
+
+                printfn "candidates: %d of %d" numberOfPressesNeeded relevantButtons.Length
+
+                let candidatePresses =
+                    combinationsOfNPresses relevantButtons numberOfPressesNeeded//  |> Seq.toList
+
+                // printfn "candidates: -> %d" candidatePresses.Length
+
+                for bs in candidatePresses do
+                    let current' = after bs current
+
+                    if isValidFor target current' then
+                        // printfn "valid %A %A" target current'
+
+                        for bs' in possiblePresses target (idx + 1) current' do
+                            yield bs @ bs'
+                    else
+                        printfn "invalid %A %A" target current'
+        }
+
+    emptySlots count
+    |> possiblePresses joltage 0
+    |> Seq.sortBy List.length
+    |> Seq.head
+    |> List.length
+    |> tap (printfn "results:\n%A")
+
+let part1 machines = machines |> List.map solve1 |> List.sum
+
+let part2 machines = machines |> List.map solve2 |> List.sum
+// let part2 machines =
+//     machines
+//     |> List.map (fun ((target, buttons, other): Machine) -> buttons.Length, List.max other)
+//     |> List.toArray
 
 lines |> part1 |> printfn "Part1: %A"
+lines |> part2 |> printfn "Part2: %A"
